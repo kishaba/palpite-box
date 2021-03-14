cb0e367641d9585570d4cb095cc34ddf58521944
Alexandre Mrus <alexandre.azevedo.mrus@gmail.com>
2021-03-14 14:37:16 -0300

    Arrumando o base 64

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,4 +1,5 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
+import { fromBase64 } from '../../utils/base64'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 export default async (req, res) => {
@@ -8,7 +9,7 @@ export default async (req, res) => {
     //console.log(process.env.SHEET_PRIVATE_KEY)
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
-      private_key: process.env.SHEET_PRIVATE_KEY,
+      private_key: fromBase64(process.env.SHEET_PRIVATE_KEY),
     })
     await doc.loadInfo()
     const sheet = doc.sheetsByIndex[2]
